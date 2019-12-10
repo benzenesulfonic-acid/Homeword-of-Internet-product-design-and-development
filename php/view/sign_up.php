271b1dbc3cffbf7a7615084ff77f967cbce966e2
benzenesulfonic-acid <36097692+benzenesulfonic-acid@users.noreply.github.com>
2019-12-10 13:54:21 +0800

    fixwithemail

--- php/view/sign_up.php
+++ php/view/sign_up.php
@@ -12,7 +12,7 @@ require_once('../logger.php');
  * error code - description
  * 401  - 用户已存在
  */
-if(isset($_POST['user_name']) && isset($_POST['password'])){
+if(isset($_POST['user_name']) && isset($_POST['password']) && isset($_POST['email'])){
     if($con=getConnent()){
         $response=new response(false,102);
         if(($result=stmt_query($con,"SELECT id FROM `user` WHERE user_name=? ",'s',$_POST['user_name']))!==false){
@@ -21,7 +21,7 @@ if(isset($_POST['user_name']) && isset($_POST['password'])){
             }
             else{
                 $password=password_hash($_POST["password"],PASSWORD_DEFAULT);
-                if($result=stmt_query($con,"INSERT INTO user(user_name,password) VALUES(?,?)",'ss',$_POST['user_name'],$password)){
+                if($result=stmt_query($con,"INSERT INTO user(user_name,password,email) VALUES(?,?,?)",'sss',$_POST['user_name'],$password,$_POST['email'])){
                     $response->set_response(true,'');
                 }
                 else{
